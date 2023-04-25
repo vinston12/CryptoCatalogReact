8417bfb9dc568cd7cb204f72145bded6b6e8919b
vinston12 <dom.boberek@gmail.com>
2023-04-25 13:14:41 +0200

    wersja bez buttona

--- src/Crypto.js
+++ src/Crypto.js
@@ -25,11 +25,10 @@ const Crypto = ({ data }) => {
         <Table>
           <caption className="caption-top">Wynik zapytania z bazy</caption>
           <thead>
-          <tr >
+          <tr className='border border-slate-300'>
             <th  className='border border-slate-300'>Id</th>
             <th  className='border border-slate-300'>User ID</th>
             <th  className='border border-slate-300'>Title</th>
-            <th  className='border border-slate-300'>Actions</th>
           </tr>
           </thead>
           <tbody>
@@ -39,35 +38,14 @@ const Crypto = ({ data }) => {
               )
               .map((item) => (
                   <React.Fragment key={item.id}>
-                   
-                    <tr className="row-color">
-              
+                    <tr className="row-color border border-slate-300" onClick={() => toggleExpansion(item.id)} ondblclick={() => Modal(item.id)}>
                       <td className='border border-slate-300'>{item.id}</td>
                       <td className='border border-slate-300'>{item.userId}</td>
                       <td className='border border-slate-300'>{item.title}</td>
-                      <td className='border border-slate-300'>
-                      <button
-                    className="btn btn-info"
-                    type="button"
-                    id={`btnMSb${item.id}`}
-                    onClick={() => toggleExpansion(item.id)}
-                    aria-expanded="false"
-                    aria-controls={`MS01b${item.id}`}
-                    aria-label={`More information about ${item.title}`}
-                  >
-                            
-                          {/* {expandedRow === item.id ? "Zwiń" : "Rozwiń"} */}
-                          </button>
-                      </td>
-                      {/* <td className='border border-slate-300'>
-                        <Modal />
-                      </td> */}
-                      
                     </tr>
-                    
                     {expandedRow === item.id && (
                         <tr>
-                          <td colSpan="4">{item.body}</td>
+                          <td className='border border-slate-300' colSpan="4">{item.body}</td>
                         </tr>
                     )}
                   </React.Fragment>
